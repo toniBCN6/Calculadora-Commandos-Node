daa55de204f952e5ea7a418870e9f2e42e06c9aa
toniBCN6 <fidel3115@gmail.com>
2022-09-10 12:55:28 +0200

    Readme agregado y modificaciones

---
+++ README.md
@@ -0,0 +1,11 @@
+# Notas:
+
+Este es mi primer programa en node
+
+```
+Opciones:
+      --help     Muestra ayuda                                        [booleano]
+      --version  Muestra número de versión                            [booleano]
+  -b, --base     Base de la tabla de multiplicar            [número] [requerido]
+  -h, --hasta    Hasta que numero multiplica la base        [número] [requerido] [defecto: 10]
+```
--- config/yargs.js
+++ config/yargs.js
@@ -2,18 +2,14 @@ const argv = require('yargs')
                 .option('b', {
                     alias: 'base',
                     type: 'number',
+                    description: 'Base de la tabla de multiplicar',
                     demandOption: true
-                })
-                .option('l', {
-                    alias: 'listar',
-                    type: 'boolean',
-                    demandOption: 'true',
-                    default: false
                 })
                 .option('h', {
                     alias: 'hasta',
                     type: 'number',
                     demandOption: 'true',
+                    description: 'Hasta que numero multiplica la base',
                     default: 10
                 })
                 .check( (argv, options) => {
